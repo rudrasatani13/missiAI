3839d4053a74c29def6f7af9c45e0b2044d51cb2
rudrasatani <rudrasatani@gmail.com>
2025-07-10 22:59:20 +0530

    update page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Yeh line zaroori hai for static export (Cloudflare Pages)
+  output: 'export', // Static export for Cloudflare Pages
   eslint: {
     ignoreDuringBuilds: true,
   },
