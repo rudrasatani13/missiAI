bb007a5aa7ee32fa3beab905e1a5bf9a5ba17ab4
rudrasatani <rudrasatani@gmail.com>
2025-07-15 17:55:50 +0530

    update page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,20 @@
-import type { Metadata } from 'next';
-import './globals.css';
+import type { Metadata } from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
   title: 'missiAI',
   description: 'The Next Gen AI Assistant',
   generator: 'missiAI Platform',
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta name="google-adsense-account" content="ca-pub-8166479565252115" />
-      </head>
       <body>{children}</body>
     </html>
-  );
+  )
 }
