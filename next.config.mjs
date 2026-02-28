272dd5ea81c6c67bbfbac387d39790ed71716d9c
rudrasatani13 <rudrasatani@gmail.com>
2026-02-28 18:32:29 +0530

    update page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Static export for Cloudflare Pages
+const nextConfig = { // Static export for Cloudflare Pages
   eslint: {
     ignoreDuringBuilds: true,
   },
