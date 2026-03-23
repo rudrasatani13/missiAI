5bd5fe9ab05b450cd1385d83916e1b57d2f9ba08
rudrasatani13 <rudrasatani@gmail.com>
2026-03-23 18:52:03 +0530

    memory updated

--- app/login/[[...sign-in]]/page.tsx
+++ app/login/[[...sign-in]]/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-export const runtime = "edge";
+// 'export const runtime = "edge";' ko completely hata dein
 
 import { SignIn } from "@clerk/nextjs"
 import { dark } from "@clerk/themes"
